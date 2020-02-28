5687fb0a35fae7a098f1ada0eeb2c7780f6fa5c1
Kiruthika Selvavinayagam <kirthi.shalom@gmail.com>
2020-02-28 08:45:13 +0530

    P153 - Find Minimum in Rotated Sorted Array with duplicates (Hard)

--- DS/Problems/LeetCode/Array/P153.m
+++ DS/Problems/LeetCode/Array/P153.m
@@ -23,6 +23,27 @@
 //Input: [4,5,6,7,0,1,2]
 //Output: 0
 
+//Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+//
+//(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+//
+//Find the minimum element.
+//
+//The array may contain duplicates.
+//
+//Example 1:
+//
+//Input: [1,3,5]
+//Output: 1
+//Example 2:
+//
+//Input: [2,2,2,0,1]
+//Output: 0
+//Note:
+//
+//This is a follow up problem to Find Minimum in Rotated Sorted Array.
+//Would allow duplicates affect the run-time complexity? How and why?
+
 #import "P153.h"
 
 @implementation P153
@@ -35,9 +56,17 @@
     }
     NSInteger low = 0, high = input.count - 1;
     NSInteger mid = (low + high)/2;
-    while(low <= high)
+    while(low < high)
     {
         mid = (low + high)/2;
+        
+        //To handle duplicate
+        if(input[low].integerValue == input[mid].integerValue && input[high].integerValue == input[mid].integerValue) // min is in left of mid , right of mid is sorted
+        {
+            low++;
+            high--;
+            continue;
+        }
         
         if(input[low].integerValue > input[mid].integerValue) // min is in left of mid , right of mid is sorted
         {
@@ -73,6 +102,16 @@
     
     P153 *case3 = [[P153 alloc] init];
     NSLog(@"%@", [case3 findMin:@[@3,@1,@2]]);
+    
+    P153 *case4 = [[P153 alloc] init];
+    NSLog(@"%@", [case4 findMin:@[@2,@2,@2,@0,@1]]);
+    
+    P153 *case5 = [[P153 alloc] init];
+    NSLog(@"%@", [case5 findMin:@[@3,@3,@1,@3]]);
+    
+    P153 *case6 = [[P153 alloc] init];
+    NSLog(@"%@", [case6 findMin:@[@1]]);
+    
 }
 
 @end
